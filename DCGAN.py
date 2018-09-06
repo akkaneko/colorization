3394c43290d4b724c280da5057fdbbdca0f6c607
akkaneko <akkaneko@stanford.edu>
2018-09-06 14:31:11 +0300

    added show and model runs

--- DCGAN.py
+++ DCGAN.py
@@ -17,14 +17,14 @@ class Discriminator(object):
     Logits from Discriminator
     """
 
-    def self.__init__(self, params):
+    def __init__(self, params):
 
         assert len(params[0]) == len(params[1]) and len(params[1]) == len(params[2]), "Please make sure param lists are the same size"
         self.num_filters = params[0]
         self.num_strides = params[1]
         self.dropouts = params[2]
 
-    def build_descriminator(self, inputs, kernel_size = None, reuse = False):
+    def discriminate(self, inputs, kernel_size = None, reuse = False):
 
         with tf.variable_scope("discriminator", reuse = reuse):
 
@@ -38,29 +38,38 @@ class Discriminator(object):
                 cur_stride = self.num_strides[ind]
                 cur_dropout = self.dropouts[ind]
 
-                conv = tf.layers.batch_normalization(conv)
-
-                conv =  tf.layers.conv2d_transpose(
+                conv =  tf.layers.conv2d(
                     inputs = conv, 
                     filters = cur_filter, 
                     kernel_size = kernel_size, 
                     strides = cur_stride, 
-                    activation = leaky_relu, 
-                    padding = 'valid"
+                    padding = "same"
                     )
 
-                if cur_dropout != 0:
-                    conv = tf.nn.dropout(conv1, cur_dropout)
+                if batch_norm:
+                    conv = tf.layers.batch_normalization(conv)
+                
+                conv = tf.nn.leaky_relu(conv)
 
-            flat = tf.layers.flatten(conv)
-            logits = tf.layers.dense(flat,1)
+                if cur_dropout != 0:
+                    conv = tf.nn.dropout(conv, cur_dropout)
+
+            
+
+            logits = tf.layers.conv2d(
+                inputs = conv,
+                filters = 1,
+                kernel_size = 4,
+                strides = 1,
+                padding = "same"
+            )
 
             return logits
 
 class Generator(object):
 
-    def self.__init__(self, encode_params, decode_params, num_channels = 3):
-        assert len(encode_params[0]) - 1) == len(decode_params[0]), "encoder must have one more  layer than decoder"
+    def __init__(self, encode_params, decode_params, num_channels = 3):
+        assert (len(encode_params[0]) - 1 == len(decode_params[0])), "encoder must have one more layer than decoder"
         assert len(encode_params[0]) == len(encode_params[1]) and len(encode_params[1]) == len(encode_params[2]), "Please make sure param lists are the same size"
         assert len(decode_params[0]) == len(decode_params[1]) and len(decode_params[1]) == len(decode_params[2]), "Please make sure param lists are the same size"
 
@@ -70,55 +79,58 @@ class Generator(object):
 
         self.dec_filters = decode_params[0]
         self.dec_strides = decode_params[1]
-        self.enc_dropout = decode_params[2]
+        self.dec_dropout = decode_params[2]
 
         self.num_channels = num_channels 
 
-    def build_generator(self, inputs, kernel_size = None, reuse = False):
-
-        conv = inputs
+    def generate(self, inputs, kernel_size = None, reuse = False):
         
         with tf.variable_scope("generator", reuse = reuse):
 
+            conv = inputs
             skip_layers = []
             
             for ind in range(len(self.enc_filters)):
 
                 cur_filter = self.enc_filters[ind]
                 cur_stride = self.enc_strides[ind]
-                cur_dropout = self.enc_dropouts[ind]
+                cur_dropout = self.enc_dropout[ind]
 
                 conv = tf.layers.conv2d(
                     inputs = conv, 
                     filters = cur_filter, 
                     kernel_size = kernel_size, 
                     strides = cur_stride, 
-                    activation = leaky_relu, 
-                    padding = 'valid"
+                    padding = "same"
                     )
-                
+
+                conv = tf.layers.batch_normalization(conv)
+                conv = tf.nn.leaky_relu(conv)
+
                 skip_layers.append(conv)
                 if cur_dropout != 0:
-                    conv = tf.nn.dropout(conv1, cur_dropout)
+                    conv = tf.nn.dropout(conv, cur_dropout)
 
             for ind in range(len(self.dec_filters)):
-                dec_filter = self.enc_filters[ind]
-                cur_stride = self.enc_strides[ind]
-                cur_dropout = self.enc_dropouts[ind]
+                cur_filter = self.dec_filters[ind]
+                cur_stride = self.dec_strides[ind]
+                cur_dropout = self.dec_dropout[ind]
 
                 conv = tf.layers.conv2d_transpose(
                 inputs = conv, 
                 filters = cur_filter, 
                 kernel_size = kernel_size, 
                 strides = cur_stride, 
-                activation = leaky_relu, 
-                padding = 'valid"
+                padding = "same"
                 )
 
-                if cur_dropout != 0:
-                    conv = tf.nn.dropout(conv1, cur_dropout)
+                conv = tf.layers.batch_normalization(conv)
+                conv = tf.nn.relu(conv)
 
-                conv = tf.concat([skip_layers[len(layers) - ind - 2], conv], axis=3)
+                if cur_dropout != 0:
+                    conv = tf.nn.dropout(conv, cur_dropout)
+
+                conv = tf.concat([skip_layers[len(skip_layers) - ind - 2], conv], axis=3)
 
             output = tf.layers.conv2d(
                 inputs = conv,
@@ -126,6 +138,7 @@ class Generator(object):
                 kernel_size = 1,
                 strides = 1,
                 activation=tf.nn.tanh,
+                padding = "same"
             )
 
             return output
@@ -133,30 +146,3 @@ class Generator(object):
         
 
 
-
-def gan_loss(logits_real, logits_fake):
-    """Compute the GAN loss.
-    
-    Inputs:
-    - logits_real: Tensor, shape [batch_size, 1], output of discriminator
-        Unnormalized score that the image is real for each real image
-    - logits_fake: Tensor, shape[batch_size, 1], output of discriminator
-        Unnormalized score that the image is real for each fake image
-    
-    Returns:
-    - D_loss: discriminator loss scalar
-    - G_loss: generator loss scalar
-    
-    HINT: for the discriminator loss, you'll want to do the averaging separately for
-    its two components, and then add them together (instead of averaging once at the very end).
-    """
-    # TODO: compute D_loss and G_loss
-    print (logits_real.shape, logits_fake.shape)
-    
-    G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = tf.ones_like(logits_fake), logits = logits_fake))
-    D_loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = tf.ones_like(logits_real), logits = logits_real)) 
-    D_loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels =  tf.zeros_like(logits_fake), logits = logits_fake))
-    D_loss = D_loss1 + D_loss2
-
-    
-    return D_loss, G_loss
--- model.py
+++ model.py
@@ -2,6 +2,12 @@ import tensorflow as tf
 import numpy as np
 from data import Cifar10Dataset
 from DCGAN import Generator, Discriminator
+from abc import abstractmethod
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+
 
 class Colorize(object):
 
@@ -9,13 +15,16 @@ class Colorize(object):
 
         self.sess = sess
         self.params = params
+        self.create_dataset(training = params['training'])
+
         
-    def build_graph(self, sess):
+    def build_graph(self):
 
         self.raw_images = tf.placeholder(tf.float32, [None, None, None, 3])
-        self.bw_images = tf.image.rgb_to_grayscale(raw_images)
+        self.bw_images = tf.image.rgb_to_grayscale(self.raw_images)
 
         lr = params['lr']
+        thresh = params['threshold']
 
         ## Center around 0 ##
         self.color_images =  (self.raw_images / 255.0) * 2 - 1
@@ -23,43 +32,51 @@ class Colorize(object):
         generator = self.create_generator()
         discriminator = self.create_discriminator()
 
-        colorized = generator.build_graph(
-            self.bw_images, 
-            kernel_size = params['kernel_size'], 
-            reuse = params['reuse'])
 
-        disc_false = discriminator.build_graph(
-            tf.concat(3, [colorized, slef.bw_images]),
+        ## Generated image: [batch_size, h, w, channels]
+        self.colorized = generator.generate(
+            self.bw_images, 
             kernel_size = params['kernel_size']
         )
 
-        disc_true  = discriminator.build_graph(
-            tf.concat(3, [self.color_images, slef.bw_images]),
-            kernel_size = params['kernel_size'],
-            reuse = True
+        ## Discriminated logits: [?, ?, ?, ?, 1]
+        disc_false = discriminator.discriminate(
+            tf.concat(axis = 3, values = [self.colorized, self.bw_images]),
+            kernel_size = params['kernel_size']
+        )
 
+        ## Discriminated logits: [?, ?, ?, ?, 1]
+        disc_true  = discriminator.discriminate(
+            tf.concat(axis = 3, values = [self.color_images, self.bw_images]),
+            kernel_size = params['kernel_size'],
+            reuse = True)
+
+        self.accuracy = self.calculate_accuracy(self.color_images, self.colorized, thresh)
         self.D_loss, self.G_loss = self.gan_loss(disc_true, disc_false)
 
-        gen_optimizer = tf.train.AdamOptimizer(learning_rate = lr).minimize(self.G_loss)
-        disc_optimizer = tf.train.AdamOptimizer(learning_rate = lr).minimize(self.D_loss)
+        self.gen_optimizer = tf.train.AdamOptimizer(learning_rate = lr).minimize(self.G_loss)
+        self.disc_optimizer = tf.train.AdamOptimizer(learning_rate = lr).minimize(self.D_loss)
+
+        self.saver = tf.train.Saver()
+
+
+    def calculate_accuracy(self, generated, original, threshold):
+
+        diff_r = tf.abs(tf.round(generated[..., 0]) - tf.round(original[..., 0]))
+        diff_g = tf.abs(tf.round(generated[..., 1]) - tf.round(original[..., 1]))
+        diff_b = tf.abs(tf.round(generated[..., 2]) - tf.round(original[..., 2]))
+
+        predr = tf.cast(tf.less_equal(diff_r, threshold), tf.float64)      
+        predg = tf.cast(tf.less_equal(diff_g, threshold), tf.float64)      
+        predb = tf.cast(tf.less_equal(diff_b, threshold), tf.float64)   
+
+        pred = predr * predg * predb
+
+        return tf.reduce_mean(pred)
+
 
     def gan_loss(self, logits_real, logits_fake):
-        """Compute the GAN loss.
-        
-        Inputs:
-        - logits_real: Tensor, shape [batch_size, 1], output of discriminator
-            Unnormalized score that the image is real for each real image
-        - logits_fake: Tensor, shape[batch_size, 1], output of discriminator
-            Unnormalized score that the image is real for each fake image
-        
-        Returns:
-        - D_loss: discriminator loss scalar
-        - G_loss: generator loss scalar
-        
-        HINT: for the discriminator loss, you'll want to do the averaging separately for
-        its two components, and then add them together (instead of averaging once at the very end).
-        """
-        
+
         G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = tf.ones_like(logits_fake), logits = logits_fake))
         D_loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = tf.ones_like(logits_real), logits = logits_real)) 
         D_loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels =  tf.zeros_like(logits_fake), logits = logits_fake))
@@ -67,7 +84,42 @@ class Colorize(object):
 
         return D_loss, G_loss
 
-        
+
+    def train(self):
+        epochs = self.params['num_epochs']
+        batch_size = self.params['batch_size']
+
+        data_gen = self.dataset.generator(batch_size)
+
+        for i in range(epochs):
+            
+            self.cur_epoch = i
+            print ("Starting Epoch Number %d") % (self.cur_epoch)
+
+            for step, batch in enumerate(data_gen):
+
+                feed_dict = {self.raw_images: batch}
+                
+                self.sess.run(
+                    [self.gen_optimizer, self.disc_optimizer],
+                    feed_dict = feed_dict)
+
+                acc, g_loss, d_loss = self.sess.run(
+                    [self.accuracy, self.G_loss, self.D_loss],
+                    feed_dict = feed_dict
+                )
+
+                print "Accuracy: %f, GenLoss: %f, DisLoss: %f" % (acc, g_loss, d_loss)
+                if step % 100 == 0:
+                    self.sample(feed_dict)
+
+    def sample(self, feed_dict):
+        fake_image = self.sess.run([self.colorized], feed_dict)
+        image = (np.squeeze(np.array(fake_image)) + 1) / 2
+        print image.shape
+        image = Image.fromarray((image[0] * 255).astype(np.uint8))
+        plt.imshow(image, interpolation='none')
+        plt.show()
 
 
     @abstractmethod
@@ -78,8 +130,50 @@ class Colorize(object):
     def create_discriminator(self):
         pass
 
+    @abstractmethod
+    def create_dataset(self, training = True):
+        pass
 
+class Color_CIFAR(Colorize):
 
-    def train(self, sess):
+    def __init__(self, datapath, sess, params):
+        self.path = datapath
+        super(Color_CIFAR, self).__init__(sess, params)
 
+    def create_dataset(self, training = True):
+        self.dataset = Cifar10Dataset(self.path, training)
 
+    def create_generator(self):
+        enc_filters = [64, 128, 256, 512, 512]
+        enc_strides = [1, 2, 2, 2, 2]
+        enc_dropout = [0, 0, 0, 0, 0]
+        encode_params = (enc_filters, enc_strides, enc_dropout)
+
+        dec_filters = [512, 256, 128, 64]
+        dec_strides = [2, 2, 2, 2]
+        dec_dropout = [0.5, 0.5, 0, 0]
+        decode_params = (dec_filters, dec_strides, dec_dropout)
+
+        return Generator(encode_params, decode_params)
+
+    def create_discriminator(self):
+        filters = [64, 128, 256, 512]
+        strides = [2, 2, 2, 1]
+        dropout = [0, 0, 0, 0]
+        params = (filters, strides, dropout)
+        
+        return Discriminator(params)
+
+params = {
+    'lr' : 0.001,
+    'threshold': 0.02,
+    'kernel_size': 2,
+    'training': True, 
+    'num_epochs': 3,
+    'batch_size': 64}
+
+with tf.Session() as sess:
+    c = Color_CIFAR('cifar-10-batches-py', sess, params)
+    c.build_graph()
+    sess.run(tf.global_variables_initializer())
+    c.train()
